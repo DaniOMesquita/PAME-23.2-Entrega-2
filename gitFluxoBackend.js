666d3660dc80b1c3f9c60aa473ff3a6828f79769
DaniOMesquita <danielaomesquita.20231@poli.ufrj.br>
2024-01-19 16:42:47 -0300

    atualização 3

--- gitFluxoBackend.js
+++ gitFluxoBackend.js
@@ -1,8 +1,9 @@
-const user=document.getElementById("user");
-const password=document.getElementById("password");
+const user = document.getElementById("user");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
 
-form.addEventListener("sumit",(event) => {
-    preventDefault();
-    alert("Cadastrado com sucesso.")
-})
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert(`Cadastrado(a) com sucesso.\n\nUsername ou email: ${user.value}\nSenha: ${password.value}`);
 
+});
