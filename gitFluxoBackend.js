c210640f6c5b468eeb78900bbd3b6518e638fadb
DaniOMesquita <danielaomesquita.20231@poli.ufrj.br>
2024-01-19 16:16:36 -0300

    2a Atualização

---
+++ gitFluxoBackend.js
@@ -0,0 +1,8 @@
+const user=document.getElementById("user");
+const password=document.getElementById("password");
+
+form.addEventListener("sumit",(event) => {
+    preventDefault();
+    alert("Cadastrado com sucesso.")
+})
+
